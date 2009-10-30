b875734fd66db98731d190814f736bd0367d9fa8
Sokolov Innokenty <r2.kenny@gmail.com>
2009-10-30 23:06:08 +0300

    Q.ajax minimal

---
+++ Q.ajax/Q.ajax.js
@@ -0,0 +1,6 @@
+/**
+ * Q.ajax Library
+ * Copyright (c) 2009 Sokolov Innokenty
+ */
+if(!Q)var Q={};Q.ajax={o:function(){var h=false;if(window.XMLHttpRequest){h=new XMLHttpRequest();}else if(window.ActiveXObject){try{h=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{h=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}return h;},get:function(u,c,t){this.a(u,null,c,'GET',t);},post:function(u,d,c,t){this.a(u,this.p(d),c,'POST',t);},a:function(u,d,c,m,t){var h=this.g();if(!h||!u)return;if(h.overrideMimeType)h.overrideMimeType('text/xml');if(!t)var t="text";if(!m)var m="GET";if(!d)var d=null;var n="timestamp="+new Date().getTime();u+=(u.indexOf("?")+1)?"&":"?";u+=n;h.open(m,u,true);if(m=="POST"){h.setRequestHeader("Content-type","application/x-www-form-urlencoded");h.setRequestHeader("Content-length",d.length);h.setRequestHeader("Connection","close");}h.onreadystatechange=function(){if(h.readyState==4){if(h.status==200){var a="";if(h.responseText)a=h.responseText;if(t=="json"){a=eval("("+a.replace(/[\n\r]/g,"")+")");}if(c)c(a);}else if(error)error(h.status);}}
+h.send(d);},p:function(o){var p=[];for(var g in o){if(o[g])p.push(g+"="+o[g]);}return p.join("&");},g:function(){return this.o();}}
