248be405c0e58dd484ca11288d12721aee98128a
Sokolov Innokenty <r2.kenny@gmail.com>
2009-11-03 23:26:27 +0300

    Q.hotkey source with shrink variables for jsmin

--- Q.hotkey/Q.hotkey.src.shrink.js
+++ Q.hotkey/Q.hotkey.src.shrink.js
@@ -15,46 +15,35 @@ Q.hotkey = {
 	},
 
 	bind: function(h, c) {
-		if (!h) return;
+		if (!h || !c) return;
 		this.k = h.toLowerCase().split("+");
 		this.c = c;
-		document.onkeydown = Q.hotkey.i;
-	},
-
-	i: function(e) {
-		e = e || window.event;
-		var c, h, k = Q.hotkey.k;
-		if (e.keyCode) c = e.keyCode;
-		else if (e.which) c = e.which;
-		var q = true;
-		eval("if(Q.hotkey.s.k"+c+") {n = false;code=Q.hotkey.s.k"+c+";};");
-		if (q) h = String.fromCharCode(c).toLowerCase();
-		else h = c;
-		var p = [], n = [], s = false, m = 0;
-		if (e.ctrlKey)	p.push("ctrl")
-		if (e.shiftKey)	p.push("shift")
-		if (e.altKey)		p.push("alt")
-		if (p.length > 1) p.sort();
-		p.push(h);
-		for (var i = 0; i < k.length; i++) {
-			if (k[i] == "ctrl") {
-				n.push("ctrl"); m++;
-			} else if (k[i] == "shift") {
-				n.push("shift"); m++;
-			} else if (k[i] == "alt") {
-				n.push("alt"); m++;
-			} else {
-				if(m > 1 && !s) {
+		document.onkeydown = function(e) {
+			e = e || window.event;
+			var c, k = Q.hotkey.k;
+			if (e.keyCode) c = e.keyCode;
+			else if (e.which) c = e.which;
+			eval("if(Q.hotkey.s.k"+c+") {c=Q.hotkey.s.k"+c+";} else {c = String.fromCharCode(c).toLowerCase();}");
+			var p = [], n = [], s = false, m = 0;
+			if (e.ctrlKey) p.push("ctrl");
+			if (e.shiftKey) p.push("shift");
+			if (e.altKey) p.push("alt");
+			if (p.length > 1) p.sort();
+			p.push(c);
+			for (var i = 0; i < k.length; i++) {
+				if (k[i] == "ctrl" || k[i] == "shift" || k[i] == "alt" ) {
+					m++;
+				} else if (m > 1 && !s) {
 					n.sort();
 					s = true;
 				}
-				n.push(k[i])
+				n.push(k[i]);
 			}
-		}
 
-		if (p.toString() == n.toString()) {
-			Q.hotkey.c();
-			return false;
+			if (p.toString() == n.toString()) {
+				Q.hotkey.c();
+				return false;
+			}
 		}
 	}
 };
