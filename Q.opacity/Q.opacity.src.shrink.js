c56743476582e29778c775c50dfaec95966eb9ad
Sokolov Innokenty <r2.kenny@gmail.com>
2009-11-12 09:57:30 +0300

    Q.opacity source with shrink variables for jsmin

---
+++ Q.opacity/Q.opacity.src.shrink.js
@@ -0,0 +1,23 @@
+/**
+ * Q.opacity Library
+ * Copyright (c) 2009 Sokolov Innokenty
+ */
+
+/**
+ * @example Q.opacity.set(elm, opacity)
+ * @desc Set opacity to elm
+ */
+if(!Q) var Q = {};
+Q.opacity = {
+	set: function(e, o) {
+		if (!e || !o) return;
+		if (typeof document.body.style.opacity == 'string') { // CSS3 (Gecko, WebKit...)
+			e.style.opacity = o;
+		} else if (document.body.filters) { // Trident (MSHTML) 5.5+
+			o *= 100;
+			var alpha = e.filters["DXImageTransform.Microsoft.alpha"] || e.filters.alpha;
+			if (alpha) alpha.opacity = o;
+			else e.style.filter += "progid:DXImageTransform.Microsoft.Alpha(opacity=" + o + ")";
+		}
+	}
+}
