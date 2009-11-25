943d874fbd8892756b5eb396c7ea8aaf4c8fa337
Sokolov Innokenty <r2.kenny@gmail.com>
2009-11-25 17:46:27 +0300

    Q.notification source for dev

---
+++ Q.notification/Q.notification.src.js
@@ -0,0 +1,48 @@
+/**
+ * Q.notification Library
+ * Copyright (c) 2009 Sokolov Innokenty
+ */
+
+if (!Q) var Q = {};
+Q.notification = {
+	sec: 4,
+	backgroundColor: "#333",
+	color: "white",
+
+	show: function(text) {
+		this.close();
+		box = document.createElement("div");
+		box.style.position = "fixed";
+		box.style.top = "10px";
+		box.style.right = "10px";
+		box.style.backgroundColor = this.backgroundColor;
+		box.style.color = this.color;
+		box.style.padding = "5px 10px";
+		box.style.borderRadius = "5px";
+		box.style.MozBorderRadius = "5px";
+		box.style.cursor = "help";
+		Q.opacity.set(box, 0.7);
+		box.innerHTML = text;
+		box.onclick = function() {
+			Q.notification.close();
+		}
+		this.t = setTimeout(
+			function() {
+				Q.notification.close();
+			}, this.sec*1000
+		);
+		this.box = box;
+		document.body.appendChild(box);
+	},
+
+	close: function() {
+		if (this.box) {
+			document.body.removeChild(this.box);
+			this.box = null;
+		}
+		if (this.t) {
+			clearTimeout(this.t);
+			this.t = null;
+		}
+	}
+}
