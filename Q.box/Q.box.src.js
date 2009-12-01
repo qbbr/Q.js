ef5ad10176e6acdfdd4d6a57acadeca27c6f4790
Sokolov Innokenty <r2.kenny@gmail.com>
2009-12-01 18:19:52 +0300

    Q.box source for dev

--- Q.box/Q.box.src.js
+++ Q.box/Q.box.src.js
@@ -11,10 +11,13 @@ if (!Q) var Q = {};
 Q.box = {
 	width: 300,
 	height: 100,
+	font: "14px Arial, Sans-serif",
 	borderColor: "#555",
 	borderWidth: 12,
+	opacity: 0.5,
 	padding: 5,
 	backgroundColor: "white",
+	color: "#333",
 	btnClose: true,
 
 	show: function(text) {
@@ -25,14 +28,12 @@ Q.box = {
 
 		var main = document.createElement("div");
 		this.main = main;
-		main.style.position = "absolute";
 		main.style.left = left + "px";
 		main.style.top = top + "px";
 		main.style.width = this.width + "px";
-		main.style.zIndex = 13;
-		main.style.fontFamily = "Arial, Sans-serif";
-		main.style.fontSize = "14px";
-		main.style.color = "#555";
+		main.style.zIndex = 93;
+		main.style.font = this.font;
+		main.style.color = this.color;
 		main.innerHTML = text;
 
 		if (this.btnClose) {
@@ -43,8 +44,6 @@ Q.box = {
 			var a = document.createElement("a");
 			a.style.color = "black";
 			a.style.fontWeight = "bold";
-			a.style.fontFamily = "Arial, Sans-serif";
-			a.style.fontSize = "14px";
 			a.style.cursor = "pointer";
 			a.onclick = function() {
 				Q.box.close();
@@ -65,26 +64,29 @@ Q.box = {
 
 		var blank = document.createElement("div");
 		this.blank = blank;
-		blank.style.position = "absolute";
 		blank.style.left = left - this.padding + "px";
 		blank.style.top = top - this.padding + "px";
 		blank.style.width = this.width + 2*this.padding + "px";
 		blank.style.backgroundColor = this.backgroundColor;
-		blank.style.zIndex = 12;
+		blank.style.zIndex = 92;
 		document.body.appendChild(blank);
 
 		var box = document.createElement("div");
 		this.box = box;
-		box.style.position = "absolute";
 		box.style.left = left - this.borderWidth - this.padding + "px";
 		box.style.top = top - this.borderWidth - this.padding + "px";
 		box.style.width = this.width + 2*this.padding + 2*this.borderWidth + "px";
 		box.style.backgroundColor = this.borderColor;
 		box.style.borderRadius = "10px";
 		box.style.MozBorderRadius = "10px";
-		box.style.zIndex = 11;
-		this.opacity(box, 0.8);
+		box.style.zIndex = 91;
+		try {
+			Q.opacity.set(box, this.opacity);
+		} catch (e) {}
+
 		document.body.appendChild(box);
+
+		main.style.position = blank.style.position = box.style.position = "absolute";
 
 		this.autoHeight();
 
@@ -99,22 +101,11 @@ Q.box = {
 		this.box.style.height = h + 2*this.padding + 2*this.borderWidth + "px";
 	},
 
-	opacity: function(elm, opacity) {
-		if (typeof document.body.style.opacity == 'string') { // CSS2 (Gecko, WebKit...)
-			elm.style.opacity = opacity;
-		} else if (document.body.filters) { // Trident (MSHTML) 5.5+
-			opacity *= 100;
-			var alpha = elm.filters["DXImageTransform.Microsoft.alpha"] || elm.filters.alpha;
-			if (alpha) alpha.opacity = opacity;
-			else elm.style.filter += "progid:DXImageTransform.Microsoft.Alpha(opacity=" + opacity + ")";
-		}
-	},
-
 	close: function() {
 		document.body.removeChild(this.box);
 		document.body.removeChild(this.blank);
 		document.body.removeChild(this.main);
-		this.unBindClose();
+		this.u();
 		this.box = this.blank = this.main = null;
 	},
 
