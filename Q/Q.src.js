0baaf54310b582e18e2d2920c65a0d91e59f0251
Sokolov Innokenty <r2.kenny@gmail.com>
2010-04-23 10:46:04 +0400

    mv browser -> Q.browser

--- Q/Q.src.js
+++ Q/Q.src.js
@@ -73,30 +73,5 @@ Q = {
 		if (!elm) return;
 		elm.style.opacity = opacity;
 		elm.style.filter = 'alpha(opacity=' + opacity * 100 + ')';
-	},
-
-	browser: function() {
-		var ua = navigator.userAgent, ie = 0, gecko = /Gecko\//.test(ua) ? ua.match(/; rv:1\.(\d+?)\.(\d)/) : 0;
-
-		if (document.all) {
-			if (window.XDomainRequest) {
-				ie = 8;
-			} else if (window.XMLHttpRequest) {
-				ie = 7;
-			} else if (document.compatMode) {
-				ie = 6;
-			} else if (window.attachEvent) {
-				ie = 5;
-			} else {
-				ie = 4;
-			}
-		}
-
-		return {
-			ie: ie,
-			gecko: gecko ? '1.' + gecko.slice(1).join('.') : 0,
-			opera: window.opera && opera.version ? opera.version()[0] : 0,
-			webkit: /AppleWebKit/.test(ua) ? ua.match(/AppleWebKit\/(\d+?\.\d+?\s)/)[1] : 0
-		};
 	}
 };
--- Q/Q.src.shrink.js
+++ Q/Q.src.shrink.js
@@ -73,30 +73,5 @@ Q = {
 		if (!e) return;
 		e.style.opacity = o;
 		e.style.filter = 'alpha(opacity=' + o * 100 + ')';
-	},
-
-	browser: function() {
-		var u = navigator.userAgent, i = 0, g = /Gecko\//.test(u) ? u.match(/; rv:1\.(\d+?)\.(\d)/) : 0;
-
-		if (document.all) {
-			if (window.XDomainRequest) {
-				i = 8;
-			} else if (window.XMLHttpRequest) {
-				i = 7;
-			} else if (document.compatMode) {
-				i = 6;
-			} else if (window.attachEvent) {
-				i = 5;
-			} else {
-				i = 4;
-			}
-		}
-
-		return {
-			ie: i,
-			gecko: g ? '1.' + g.slice(1).join('.') : 0,
-			opera: window.opera && opera.version ? opera.version()[0] : 0,
-			webkit: /AppleWebKit/.test(u) ? u.match(/AppleWebKit\/(\d+?\.\d+?\s)/)[1] : 0
-		};
 	}
 };
